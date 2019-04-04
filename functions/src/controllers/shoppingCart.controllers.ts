6df53c7ad712b97e7775056dd76d5f0927e62a20
Ismael Sayas Arrieta <isayas@bancolombia.com.co>
2019-04-04 16:04:18 -0500

    storage configuration

--- functions/src/controllers/shoppingCart.controllers.ts
+++ functions/src/controllers/shoppingCart.controllers.ts
@@ -11,17 +11,19 @@ const addShoppingCart = (req: any, res: any) => {
 };
 
 const getShoppingCartDetail = (req: any, res: any) => {
-  CART_SERVICES.getCart(req.params.id).then(cart => {
-    res.status(200).send(createResponse("Ok", cart));
-  });
+  CART_SERVICES.getCart(req.params.id)
+    .then(cart => {
+      res.status(200).send(createResponse("Ok", cart));
+    })
+    .catch(err => res.status(err.status ? err.status : 500).send(err));
 };
 
 const updateShoppingCart = (req: any, res: any) => {
-  CART_SERVICES.updateCart(req.params.id, pick(req.body, "products")).then(
-    carts => {
+  CART_SERVICES.updateCart(req.params.id, pick(req.body, "products"))
+    .then(carts => {
       res.status(200).send(createResponse("Cart updated", carts));
-    }
-  );
+    })
+    .catch(err => res.status(err.status ? err.status : 500).send(err));
 };
 
 export { addShoppingCart, getShoppingCartDetail, updateShoppingCart };
