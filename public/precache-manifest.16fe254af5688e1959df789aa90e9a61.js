3faa677b8976b657abafeae0a461fd47c32777c8
Ismael Sayas Arrieta <isayas@bancolombia.com.co>
2019-04-15 17:43:39 -0500

    Market place bild added

---
+++ public/precache-manifest.16fe254af5688e1959df789aa90e9a61.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "25bfb5d37431e7baee97688f1b9ece22",
+    "url": "/static/media/cart.25bfb5d3.svg"
+  },
+  {
+    "revision": "a290c616ce4a7c6294bc",
+    "url": "/static/css/main.8904f15f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27dcdcdb959b31df116c",
+    "url": "/static/js/2.72f3d864.chunk.js"
+  },
+  {
+    "revision": "e1c53c37167c91cc1ab94a1b7388de95",
+    "url": "/static/media/header_background.e1c53c37.jpg"
+  },
+  {
+    "revision": "a290c616ce4a7c6294bc",
+    "url": "/static/js/main.50a5001d.chunk.js"
+  },
+  {
+    "revision": "7b88f4e912498f14f257dbfd54cee720",
+    "url": "/static/media/facebook.7b88f4e9.svg"
+  },
+  {
+    "revision": "0dc461871f8af99c59e0ae57ca32cabc",
+    "url": "/static/media/twitter.0dc46187.svg"
+  },
+  {
+    "revision": "64d7e2bdf9de5e0c841631676d04987b",
+    "url": "/static/media/linkedin.64d7e2bd.svg"
+  },
+  {
+    "revision": "2788ea4169507f5f2c0fe05ac057df2e",
+    "url": "/static/media/plus-button.2788ea41.svg"
+  },
+  {
+    "revision": "848970aff1c0f1c71e0eec5681efdc56",
+    "url": "/index.html"
+  }
+];
