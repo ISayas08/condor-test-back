315dee3730da2b437cb10329c63f322020dfe4f9
Ismael Sayas Arrieta <isayas@bancolombia.com.co>
2019-04-15 17:42:24 -0500

    Delete cart endpoints

--- functions/src/controllers/index.ts
+++ functions/src/controllers/index.ts
@@ -1,5 +1,4 @@
 import * as USERS_CONTROLLERS from "./users.controllers";
 import * as PRODUCTS_CONTROLLERS from "./products.controller";
-import * as CART_CONTROLLERS from "./shoppingCart.controllers";
 
-export { USERS_CONTROLLERS, PRODUCTS_CONTROLLERS, CART_CONTROLLERS };
+export { USERS_CONTROLLERS, PRODUCTS_CONTROLLERS };
